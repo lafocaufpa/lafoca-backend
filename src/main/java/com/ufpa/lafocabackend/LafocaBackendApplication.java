ced886800e6193079b0eb57634a64ac802a44458
Wenderson Santos <guto15santos@gmail.com>
2023-10-25 22:47:30 -0300

    Projeto Iniciado

---
+++ src/main/java/com/ufpa/lafocabackend/LafocaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ufpa.lafocabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LafocaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LafocaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ufpa/lafocabackend/LafocaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufpa.lafocabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LafocaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
