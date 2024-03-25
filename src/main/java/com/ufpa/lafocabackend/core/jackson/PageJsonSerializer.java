75b7205ef27ba6afc52e753fb39ab65c71144f7d
Wenderson Santos <wgustavo.dev@gmail.com>
2024-03-25 16:50:01 -0300

    Resource pagination customization

--- src/main/java/com/ufpa/lafocabackend/core/jackson/PageJsonSerializer.java
+++ src/main/java/com/ufpa/lafocabackend/core/jackson/PageJsonSerializer.java
@@ -4,6 +4,7 @@ import org.codehaus.jackson.JsonGenerator;
 import org.codehaus.jackson.JsonProcessingException;
 import org.codehaus.jackson.map.JsonSerializer;
 import org.codehaus.jackson.map.SerializerProvider;
+import org.springframework.boot.jackson.JsonComponent;
 import org.springframework.data.domain.Page;
 
 import java.io.IOException;
@@ -13,14 +14,20 @@ import java.io.IOException;
  * para reduzir as informações desnecessárias padrão de um Page
  *
  */
-//@JsonComponent
+@JsonComponent
 public class PageJsonSerializer extends JsonSerializer<Page<?>> {
     @Override
     public void serialize(Page<?> objects, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException, JsonProcessingException {
 
         jsonGenerator.writeStartObject();
 
-        jsonGenerator.writeObject("");
+        jsonGenerator.writeObjectField("content", objects.getContent());
+        jsonGenerator.writeNumberField("size", objects.getSize());
+        jsonGenerator.writeNumberField("totalElements", objects.getTotalElements());
+        jsonGenerator.writeNumberField("totalPages", objects.getTotalPages());
+        jsonGenerator.writeNumberField("number", objects.getNumber());
+
+        jsonGenerator.writeEndObject();
 
     }
 }
