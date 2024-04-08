4a2aae1412cbcb649d784d615c94fb0d0d7fad34
Wenderson Santos <wgustavo.dev@gmail.com>
2024-04-08 12:34:04 -0300

    [FEATURE] Reading custom JWT information from the Resource Server

--- src/main/java/com/ufpa/lafocabackend/core/security/ResourceServerConfig.java
+++ src/main/java/com/ufpa/lafocabackend/core/security/ResourceServerConfig.java
@@ -6,7 +6,15 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -21,8 +29,40 @@ public class ResourceServerConfig {
                 .csrf().disable()
                 .cors()
                 .and()
-                .oauth2ResourceServer().jwt();
+                .oauth2ResourceServer().jwt().jwtAuthenticationConverter(jwtAuthenticationConverter());
 
         return httpSecurity.formLogin(Customizer.withDefaults()).build();
+    }
+
+
+    /**
+     * Um converter customizado para ler authorities do Authorization Server.
+     * O Resource Server precisa saber lidar com as custom claims do Auth, para isso
+     * é necessário converte-las em um tipo que o mesmo consiga identificar e garantir o acesso
+     * aos endpoints e métodos protegidos.
+     * @return
+     */
+    private JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+
+        converter.setJwtGrantedAuthoritiesConverter(jwt -> {
+            List<String> authorities = jwt.getClaimAsStringList("authorities");
+
+            if (authorities == null) {
+                return Collections.emptyList();
+            }
+
+            JwtGrantedAuthoritiesConverter authoritiesConverter = new JwtGrantedAuthoritiesConverter();
+            Collection<GrantedAuthority> grantedAuthorities = authoritiesConverter.convert(jwt);
+
+            grantedAuthorities.addAll(authorities
+                    .stream()
+                    .map(SimpleGrantedAuthority::new)
+                    .toList());
+
+            return grantedAuthorities;
+        });
+
+        return converter;
     }
 }
