18c79443d9ce9ccb2df769f2c05c2dffb0b611fb
wenderson gustavo <wgustavo.dev@gmail.com>
2024-07-28 19:40:50 -0300

    Adicionando domínio a configuração de cors

--- src/main/java/com/ufpa/lafocabackend/core/security/CorsConfig.java
+++ src/main/java/com/ufpa/lafocabackend/core/security/CorsConfig.java
@@ -38,7 +38,8 @@ public class CorsConfig {
                 "https://lafoca.com.br",
                 "http://localhost:3000",
                 "http://10.0.0.227:3000",
-                "http://192.168.1.7:3000"
+                "http://192.168.1.7:3000",
+                "https://lafoca-frontend.vercel.app"
         );
 
         config.setAllowedOrigins(allowedOrigins);
